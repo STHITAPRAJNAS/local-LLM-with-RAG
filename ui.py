c5f7196d9601b32b3de71de31bd15abdc6243d8c
Sthitaprajna Sahoo <papu.sahoo@gmail.com>
2024-06-27 01:17:46 -0400

    Streamlit enhancement

--- ui.py
+++ ui.py
@@ -1,17 +1,13 @@
 import streamlit as st
 import os
+import tempfile
 
 from langchain_community.llms import Ollama
 from document_loader import load_documents_into_database
-
 from models import get_list_of_models
-
 from llm import getStreamingChain
 
-
 EMBEDDING_MODEL = "nomic-embed-text"
-PATH = "Research"
-
 
 st.title("Local LLM with RAG 📚")
 
@@ -26,27 +22,27 @@ if st.session_state.get("ollama_model") != selected_model:
     st.session_state["ollama_model"] = selected_model
     st.session_state["llm"] = Ollama(model=selected_model)
 
+# File uploader for multiple files
+uploaded_files = st.sidebar.file_uploader("Choose files", accept_multiple_files=True)
 
-# Folder selection
-folder_path = st.sidebar.text_input("Enter the folder path:", PATH)
+if uploaded_files:
+    if st.sidebar.button("Index Documents"):
+        if "db" not in st.session_state:
+            with st.spinner("Creating embeddings and loading documents into Chroma..."):
+                # Create a temporary directory to store uploaded files
+                with tempfile.TemporaryDirectory() as temp_dir:
+                    for file in uploaded_files:
+                        file_path = os.path.join(temp_dir, file.name)
+                        with open(file_path, "wb") as f:
+                            f.write(file.getbuffer())
 
-if folder_path:
-    if not os.path.isdir(folder_path):
-        st.error(
-            "The provided path is not a valid directory. Please enter a valid folder path."
-        )
-    else:
-        if st.sidebar.button("Index Documents"):
-            if "db" not in st.session_state:
-                with st.spinner(
-                    "Creating embeddings and loading documents into Chroma..."
-                ):
+                    # Load documents from the temporary directory
                     st.session_state["db"] = load_documents_into_database(
-                        EMBEDDING_MODEL, folder_path
+                        EMBEDDING_MODEL, temp_dir
                     )
-                st.info("All set to answer questions!")
+            st.info("All set to answer questions!")
 else:
-    st.warning("Please enter a folder path to load documents into the database.")
+    st.warning("Please upload files to load documents into the database.")
 
 # Initialize chat history
 if "messages" not in st.session_state:
